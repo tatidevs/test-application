daf0c1b37f524d74c3e9089b4ed4d2246c5f211c
Tatiana <tatianathedeveloper@gmail.com>
2021-03-04 12:29:19 +0300

    check rule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'b3a-root',
+  selector: 'b3-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
