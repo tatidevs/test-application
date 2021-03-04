aa36ed62d30a0281f2564b41cdd4b7dc3ea2dbe0
Tatiana <tatianathedeveloper@gmail.com>
2021-03-04 12:25:14 +0300

    fix lint problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
+  selector: 'b3a-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
